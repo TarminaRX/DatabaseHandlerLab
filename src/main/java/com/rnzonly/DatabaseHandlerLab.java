5fc2a88538a22afe7a23a724635f66cc43bbe2f2
TarminaRX <tarminarobes@hotmail.com>
2025-02-01 18:16:57 +0800

    first quarter done.

--- src/main/java/com/rnzonly/DatabaseHandlerLab.java
+++ src/main/java/com/rnzonly/DatabaseHandlerLab.java
@@ -4,6 +4,7 @@ import java.sql.*;
 
 public class DatabaseHandlerLab {
   private Connection sqlCon;
+  private PreparedStatement queryToExecute;
 
   DatabaseHandlerLab(String patternDatabase) {
     String connStr = "jdbc:sqlite:resources/" + patternDatabase;
@@ -15,8 +16,63 @@ public class DatabaseHandlerLab {
     }
   }
 
-  public static void main(String[] args) { 
-    DatabaseHandlerLab dbh = new DatabaseHandlerLab("students.db");
-    System.out.println("Hello World");
+  private String formatSqlString(String fMat) { return "\"" + fMat + "\""; }
+
+  private Student resultToStudent(ResultSet stud) throws SQLException {
+    Student bufStud = new Student(
+        stud.getString("student_number"), stud.getString("student_fname"),
+        stud.getString("student_mname"), stud.getString("student_lname"),
+        stud.getString("student_sex"), stud.getString("student_birth"),
+        stud.getString("student_department"), stud.getString("student_address"),
+        stud.getInt("student_start"), stud.getInt("student_units"));
+    return bufStud;
+  }
+
+  void initializeStudents() throws SQLException {
+    queryToExecute = sqlCon.prepareStatement("DROP TABLE IF EXISTS Students");
+    String sqlTwo = "CREATE TABLE Students (\n"
+                    + "    student_number TEXT NOT NULL,\n"
+                    + "    student_fname TEXT NOT NULL,\n"
+                    + "    student_mname TEXT,\n"
+                    + "    student_lname TEXT NOT NULL,\n"
+                    + "    student_sex TEXT NOT NULL,\n"
+                    + "    student_birth TEXT NOT NULL,\n"
+                    + "    student_start INTEGER NOT NULL,\n"
+                    + "    student_department TEXT NOT NULL,\n"
+                    + "    student_units INTEGER NOT NULL,\n"
+                    + "    student_address TEXT,\n"
+                    +
+                    "    CONSTRAINT Students_PK PRIMARY KEY (student_number)\n"
+                    + ");";
+
+    queryToExecute.executeUpdate();
+    queryToExecute = sqlCon.prepareStatement(sqlTwo);
+    queryToExecute.executeUpdate();
+  }
+
+  Student getStudent(String studentNumber) throws SQLException {
+    queryToExecute = sqlCon.prepareStatement(
+        "SELECT * FROM Students s WHERE s.student_number = ?");
+    queryToExecute.setString(1, studentNumber);
+    ResultSet r = queryToExecute.executeQuery();
+    while (r.next()) {
+      return resultToStudent(r);
+    }
+    return null;
+  }
+
+  Student getStudent(String studentFname, String studentMname, String studentLname) { 
+    return null;
+  }
+
+  public static void main(String[] args) {
+    try {
+      DatabaseHandlerLab dbh = new DatabaseHandlerLab("students.db");
+      //dbh.initializeStudents();
+      Student a = dbh.getStudent("12340105678");
+      System.out.println("Hello World");
+    } catch (Exception e) {
+      System.out.println(e.toString());
+    }
   }
 }
---
+++ src/main/java/com/rnzonly/Student.java
@@ -0,0 +1,80 @@
+package com.rnzonly;
+
+public class Student {
+  private String student_number, student_fname, student_mname, student_lname, student_sex, student_birth, student_department, student_address;
+  private Integer student_start, student_units;
+  public Student(String student_number, String student_fname, String student_mname, String student_lname,
+      String student_sex, String student_birth, String student_department, String student_address,
+      Integer student_start, Integer student_units) {
+    this.student_number = student_number;
+    this.student_fname = student_fname;
+    this.student_mname = student_mname;
+    this.student_lname = student_lname;
+    this.student_sex = student_sex;
+    this.student_birth = student_birth;
+    this.student_department = student_department;
+    this.student_address = student_address;
+    this.student_start = student_start;
+    this.student_units = student_units;
+  }
+  public String getStudent_number() {
+    return student_number;
+  }
+  public void setStudent_number(String student_number) {
+    this.student_number = student_number;
+  }
+  public String getStudent_fname() {
+    return student_fname;
+  }
+  public void setStudent_fname(String student_fname) {
+    this.student_fname = student_fname;
+  }
+  public String getStudent_mname() {
+    return student_mname;
+  }
+  public void setStudent_mname(String student_mname) {
+    this.student_mname = student_mname;
+  }
+  public String getStudent_lname() {
+    return student_lname;
+  }
+  public void setStudent_lname(String student_lname) {
+    this.student_lname = student_lname;
+  }
+  public String getStudent_sex() {
+    return student_sex;
+  }
+  public void setStudent_sex(String student_sex) {
+    this.student_sex = student_sex;
+  }
+  public String getStudent_birth() {
+    return student_birth;
+  }
+  public void setStudent_birth(String student_birth) {
+    this.student_birth = student_birth;
+  }
+  public String getStudent_department() {
+    return student_department;
+  }
+  public void setStudent_department(String student_department) {
+    this.student_department = student_department;
+  }
+  public String getStudent_address() {
+    return student_address;
+  }
+  public void setStudent_address(String student_address) {
+    this.student_address = student_address;
+  }
+  public Integer getStudent_start() {
+    return student_start;
+  }
+  public void setStudent_start(Integer student_start) {
+    this.student_start = student_start;
+  }
+  public Integer getStudent_units() {
+    return student_units;
+  }
+  public void setStudent_units(Integer student_units) {
+    this.student_units = student_units;
+  }
+}
