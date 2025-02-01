810e791cbbf12f6d70f4ec1b7690759902ab907c
TarminaRX <tarminarobes@hotmail.com>
2025-02-01 16:33:42 +0800

    Fix stuff.

--- src/main/java/com/rnzonly/DatabaseHandlerLab.java
+++ src/main/java/com/rnzonly/DatabaseHandlerLab.java
@@ -1,13 +1,22 @@
 package com.rnzonly;
 
-/**
- * Hello world!
- *
- */
-public class DatabaseHandlerLab 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.sql.*;
+
+public class DatabaseHandlerLab {
+  private Connection sqlCon;
+
+  DatabaseHandlerLab(String patternDatabase) {
+    String connStr = "jdbc:sqlite:resources/" + patternDatabase;
+    try {
+      sqlCon = DriverManager.getConnection(connStr);
+    } catch (SQLException e) {
+      System.err.println("Failed to create connection");
+      System.err.println(e.toString());
     }
+  }
+
+  public static void main(String[] args) { 
+    DatabaseHandlerLab dbh = new DatabaseHandlerLab("students.db");
+    System.out.println("Hello World");
+  }
 }
