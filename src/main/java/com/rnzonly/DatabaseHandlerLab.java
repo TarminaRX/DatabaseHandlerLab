af9b33a4b7bfbeb1a9dbd992fad41b2c22abee13
TarminaRX <tarminarobes@hotmail.com>
2025-02-01 20:05:23 +0800

    Update another quarter done.

--- src/main/java/com/rnzonly/DatabaseHandlerLab.java
+++ src/main/java/com/rnzonly/DatabaseHandlerLab.java
@@ -1,6 +1,8 @@
 package com.rnzonly;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseHandlerLab {
   private Connection sqlCon;
@@ -15,8 +17,32 @@ public class DatabaseHandlerLab {
       System.err.println(e.toString());
     }
   }
+  
+  private void checkStudentNumber(String number) throws Exception {
+    boolean error = false;
+    if (number.length() != 11) {
+      throw new RuntimeException("Not a valid student number");
+    }
 
-  private String formatSqlString(String fMat) { return "\"" + fMat + "\""; }
+    for (int i = 0; i < number.length(); i++) {
+      if (!(number.charAt(i) >= 48 && number.charAt(i) <= 57)) {
+        error = true;
+        break;
+      }
+      if ((i == 4 || i == 6) && number.charAt(i) != '0') {
+        error = true;
+        break;
+      }
+      if (i == 5 && number.charAt(i) != '1') {
+        error = true;
+        break;
+      }
+    }
+
+    if (error) {
+      throw new RuntimeException("Not a valid student number");
+    }
+  }
 
   private Student resultToStudent(ResultSet stud) throws SQLException {
     Student bufStud = new Student(
@@ -61,15 +87,53 @@ public class DatabaseHandlerLab {
     return null;
   }
 
-  Student getStudent(String studentFname, String studentMname, String studentLname) { 
+  Student getStudent(String studentFname, String studentMname,
+                     String studentLname) throws SQLException {
+    queryToExecute = sqlCon.prepareStatement(
+        "SELECT * FROM Students s WHERE s.student_fname = ? AND "
+        + "s.student_mname = ? AND s.student_lname = ?");
+    queryToExecute.setString(1, studentFname);
+    queryToExecute.setString(2, studentMname);
+    queryToExecute.setString(3, studentLname);
+    ResultSet r = queryToExecute.executeQuery();
+    while (r.next()) {
+      return resultToStudent(r);
+    }
     return null;
   }
+
+  Student[] getStudents() throws SQLException {
+    queryToExecute = sqlCon.prepareStatement("SELECT * FROM Students");
+    ResultSet r = queryToExecute.executeQuery();
+    List<Student> tempL = new ArrayList<>();
+    while (r.next()) {
+      tempL.add(resultToStudent(r));
+    }
+    return tempL.toArray(new Student[0]);
+  }
+
+  boolean removeStudent(String studentNumber) throws SQLException {
+    queryToExecute = sqlCon.prepareStatement("DELETE FROM Students s WHERE s.student_number = ?");
+    queryToExecute.setString(1, studentNumber);
+    return false;
+  } 
+
+
+
+
+  //void checkDateNumber(String dateStr) throws Exception {
+  //  String[] splittedStr = dateStr.trim().split("-");
+  //  if 
+  //}
+
+  boolean insertStudent(Student newStudent) { return false; }
 
   public static void main(String[] args) {
     try {
       DatabaseHandlerLab dbh = new DatabaseHandlerLab("students.db");
-      //dbh.initializeStudents();
+      // dbh.initializeStudents();
       Student a = dbh.getStudent("12340105678");
+      Student[] b = dbh.getStudents();
       System.out.println("Hello World");
     } catch (Exception e) {
       System.out.println(e.toString());
