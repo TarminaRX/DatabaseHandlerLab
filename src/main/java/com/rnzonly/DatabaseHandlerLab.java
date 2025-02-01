aaebaa8459abb7012b8c060895dfca3c1ae4f053
TarminaRX <tarminarobes@hotmail.com>
2025-02-01 16:18:17 +0800

    Init files.

---
+++ src/main/java/com/rnzonly/DatabaseHandlerLab.java
@@ -0,0 +1,13 @@
+package com.rnzonly;
+
+/**
+ * Hello world!
+ *
+ */
+public class DatabaseHandlerLab 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
